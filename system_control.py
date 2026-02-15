8eb4483ee600bd93399e59003463a8ead72ea840
TNMY <119494723+TNMY-3010@users.noreply.github.com>
2026-02-15 19:47:31 +0530

    Update system_control.py

--- system_control.py
+++ system_control.py
@@ -1,1 +1,9 @@
+import os
+
+def open_application(text):
+    if "chrome" in text:
+        os.system("start chrome")
+        return "Opening Chrome..."
+    else:
+        return "Application not recognized."
 
