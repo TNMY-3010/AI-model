42bcb2cb929166c4b57bed13e043f2423ec050f2
TNMY <119494723+TNMY-3010@users.noreply.github.com>
2026-02-15 19:47:00 +0530

    Update intent_classifier.py

--- intent_classifier.py
+++ intent_classifier.py
@@ -1,1 +1,12 @@
+def classify_intent(text):
+    text = text.lower()
+
+    if "open" in text:
+        return "open_app"
+    elif "study" in text:
+        return "study_mode"
+    elif "save" in text:
+        return "save_memory"
+    else:
+        return "unknown"
 
